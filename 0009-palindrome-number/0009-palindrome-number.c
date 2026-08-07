0c439d18ffe0f64680b6e4aff05d4ba740579a2d
Adarsh shukla <adarshbhai917963@gmail.com>
2026-08-07 22:02:03 +0530

    Time: 0 ms (100%), Space: 8.9 MB (64.45%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,13 @@
+bool isPalindrome(int x) {
+    long rev = 0,rem;
+    int temp = x;
+    if(x<0) return false;
+    while(x  != 0){
+        rem = x % 10;
+        rev = rev * 10 + rem;
+        x = x/10;
+    }
+    if(temp == rev ) return true;
+    else  return false;
+    
+}
