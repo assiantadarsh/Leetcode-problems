85f36451f3fa19c33930b6f1fc0796f68f49ce46
Adarsh shukla <adarshbhai917963@gmail.com>
2026-08-10 21:51:54 +0530

    Time: 4 ms (65.81%), Space: 13.4 MB (53.84%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -6,12 +6,13 @@ public:
 
       int i= 0; 
       int j = n;
-
-      while(i < n && j < 2 * n){
+        int st = 0;
+      while(st < n){
         ans.push_back(nums[i]);
         ans.push_back(nums[j]);
         i++;
         j++;
+        st++;
       }
 
       return ans;  
