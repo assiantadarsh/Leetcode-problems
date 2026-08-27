8f016caf716b12b47c6e591d57d97ea1257c8e05
Adarsh shukla <adarshbhai917963@gmail.com>
2026-08-27 22:46:29 +0530

    Time: 2 ms (43.75%), Space: 8.4 MB (63.45%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+
+        int n = word1.size();
+        int m = word2.size();
+
+        string ans = "";
+
+        for(int i = 0 ; i < max(n , m);i++){
+
+            if(i < n) ans.push_back(word1[i]);
+            if(i < m) ans.push_back(word2[i]);
+            
+        }
+
+        return ans;
+    }
+};
